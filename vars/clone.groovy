0e1ffcf83c5c667b86cbb298ee7a9d6d2f3fca44
Shivam Mishra <shivam9878522087@gmail.com>
2025-04-19 10:50:43 +0530

    added cloning from git repository jenkins shared library

---
+++ vars/clone.groovy
@@ -0,0 +1,5 @@
+def call(String gitUrl, String branch) {
+    echo "Pulling the code from git repository..."
+    git url: "${gitUrl}", branch: "${branch}"
+    echo "Successfully pulled code from git repository..."
+}
