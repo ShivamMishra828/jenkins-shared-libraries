ce891166f8574a884b1d9425b7eef852da73ce4c
Shivam Mishra <shivam9878522087@gmail.com>
2025-09-09 19:44:42 +0530

    fix: fixed docker_push and trivy scan library

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,8 @@
-def call(String Project, String ImageTag) {
-  withCredentials([usernamePassword(credentialsId: 'dockerCred', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+def call(String ProjectName, String ImageTag) {
+  withCredentials([usernamePassword(credentialsId: 'dockerCred', passwordVariable: 'dockerHubUser', usernameVariable: 'dockerHubPass')]) {
+      sh """
+        docker login -u ${dockerHubUser} -p ${dockerHubPass}
+        docker push ${dockerHubUser}/${ProjectName}:${ImageTag}
+      """
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,3 @@
 def call() {
-    sh "trivy fs ."
+    sh "trivy fs --exit-code 1 --severity HIGH,CRITICAL ."
 }
